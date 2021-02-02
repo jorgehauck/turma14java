0683e2f080c744d27fe49c96438be8123782bcfd
jorgehauck <jorge.hauck1997@outlook.com>
2021-02-01 22:09:04 -0300

    Método de linha inserido.

--- java/aulas/src/exercicios2POO/TestaVendedor.java
+++ java/aulas/src/exercicios2POO/TestaVendedor.java
@@ -8,22 +8,31 @@ public class TestaVendedor {
 		Scanner leia = new Scanner(System.in);
 		
 		Vendedor vendedor = new Vendedor();
-		System.out.println("Ol·, digite o seu nome: ");
+		System.out.println("Ol√°, digite o seu nome: ");
 		vendedor.nome = leia.next();
 		vendedor.setNome(vendedor.nome);
 		System.out.println("Informe o valor de vendas: ");
 		vendedor.valorVendas = leia.nextDouble();
 		vendedor.setValorVendas(vendedor.valorVendas);
-		System.out.println("Informe a sua comiss„o: ");
+		System.out.println("Informe a sua comiss√£o: ");
 		vendedor.comissao = leia.nextDouble();
 		vendedor.setComissao(vendedor.comissao);
-		
-		System.out.printf("Sal·rio com comiss„o: R$ %.2f",vendedor.retornaComissao(vendedor.getValorVendas(), vendedor.getComissao()));
+		linha(30);
+		System.out.println();
+		System.out.printf("Sal√°rio com comiss√£o: R$ %.2f",vendedor.retornaComissao(vendedor.getValorVendas(), vendedor.getComissao()));
 		
 		
 		
 		leia.close();
 
+	}
+	
+	public static void linha(int tamanho)
+	{
+		for(int i = 0; i<tamanho; i++)
+		{
+			System.out.print("‚ïê");
+		}
 	}
 
 }
